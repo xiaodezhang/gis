4c2ad9914e26df98d16982a555ff362057dcc7b7
zhangxiaode <zhang_xiaode@163.com>
2017-08-05 16:45:22 +0800

    mobile control,standing book

--- baotou/urls.py
+++ baotou/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
         url(r'^$',views.index,name='index'),
         url(r'^terminal_data',views.terminal_data),
+        url(r'^standing_book_chart',views.standing_book_chart),
+        url(r'^standing_book_table',views.standing_book_table),
         ]
--- baotou/views.py
+++ baotou/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 import pymssql
@@ -8,11 +10,13 @@ serverip = "192.168.50.108"
 user = "sa"
 password = "CeTc50"
 db_name = "sde"
+db_hf_name = "sde_hf"
 
 # Create your views here.
-def sql_server_connect():
+def sql_server_connect(flag):
     try:
-        conn = pymssql.connect(serverip,user,password,db_name)
+        db = db_name if flag else db_hf_name
+        conn = pymssql.connect(serverip,user,password,db)
         cursor = conn.cursor(as_dict=True)
         return cursor
     except:
@@ -26,7 +30,21 @@ def index(request):
 
 def terminal_data(request):
 
-    cursor = sql_server_connect()
+    cursor = sql_server_connect(True)
     cursor.execute("SELECT DISTINCT tmlid,tmlname,phyid FROM sde.dbo.DataRtu")
     data_json = json.dumps(cursor.fetchall())
-    return JsonResponse(data_json,safe=False);
+    return JsonResponse(data_json,safe=False)
+
+def standing_book_chart(request):
+
+    cursor = sql_server_connect(False)
+    cursor.execute("SELECT 维护所,COUNT(*) AS 数量 FROM sde_hf.dbo.jkx_taizhang GROUP BY 维护所") 
+    data_json = json.dumps(cursor.fetchall())
+    return JsonResponse(data_json,safe=False)
+
+def standing_book_table(request):
+
+    cursor = sql_server_connect(False)
+    cursor.execute("SELECT * FROM sde_hf.dbo.jkx_taizhang") 
+    data_json = json.dumps(cursor.fetchall())
+    return JsonResponse(data_json,safe=False)
