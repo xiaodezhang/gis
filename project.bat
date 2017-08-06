7e176abc9f94173f46bd9f01a92451e5d507b212
zhangxiaode <zhang_xiaode@163.com>
2017-08-06 14:31:39 +0800

    add the project file for gvim

---
+++ project.bat
@@ -0,0 +1,1 @@
+gvim %cd%\baotou\views.py %cd%\baotou\urls.py %cd%\baotou\templates\baotou\index.html %cd%\baotou\static\baotou\js\index.js %cd%\baotou\static\baotou\js\map.js %cd%\baotou\static\baotou\css\style.css
